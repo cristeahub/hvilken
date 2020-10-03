d44e12c55762f816f935f1f04c1b93187df00826
Alexsey Ramzaev <ekstazu@gmail.com>
2020-10-04 00:02:01 +0400

    add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Data.Time
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  -- quickCheck weekNumberFromDate
+  t <- getCurrentTime
+  putStrLn ""
+  putStrLn $ if weekNumberFromDate t <= 52 then "OK" else "FAIL!"
+  return ()
