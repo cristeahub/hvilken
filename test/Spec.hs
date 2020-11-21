6df99e0ab7379f5ecc5c76d572fc0d706ec26313
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2020-11-21 13:50:35 +0100

    Update test suite to test more specific cases
    This fixes #12.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,23 @@
 import Data.Time
+import Data.Time.Calendar
 import Lib
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   describe "Prelude.head" $ do
-    it "makes sure weekNumberFromDate is less than or equal to 52" $ do
+    it "Week number 53" $
+      let t = UTCTime (fromGregorian 2009 12 31) (secondsToDiffTime 0)
+       in weekNumberFromDate t `shouldSatisfy` (== 53)
+
+    it "Week number 52" $
+      let t = UTCTime (fromGregorian 2011 12 31) (secondsToDiffTime 0)
+       in weekNumberFromDate t `shouldSatisfy` (== 52)
+
+    it "Week number 1" $
+      let t = UTCTime (fromGregorian 2007 12 31) (secondsToDiffTime 0)
+       in weekNumberFromDate t `shouldSatisfy` (== 1)
+
+    it "Random week number less than or equal 53" $ do
       t <- getCurrentTime
-      weekNumberFromDate t `shouldSatisfy` (<= 52)
+      weekNumberFromDate t `shouldSatisfy` (<= 53)
